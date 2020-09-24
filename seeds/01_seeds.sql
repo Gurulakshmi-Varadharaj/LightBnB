26ff6923efbd785dcfa103e991456d1b0755c9c0
Gurulakshmi Varadharaj <gurulakshmi.varadharaj@gmail.com>
2020-09-24 03:04:59 +0000

    Insert Data into tables

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (id, name, email, password)
+VALUES ('Eva Stanely', 'sebastinguerra@ymail.coms', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+VALUES ('Louisa Meyer', 'jacksonrose@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.')
+VALUES ('Dominic Parks', 'victoriablackwell@outlook.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
