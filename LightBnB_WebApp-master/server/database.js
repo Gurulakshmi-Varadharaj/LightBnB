41a1fa85a92e20a21f9df0a97ad2bdc666b9a252
Gurulakshmi Varadharaj <gurulakshmi.varadharaj@gmail.com>
2020-09-25 12:38:05 +0000

    LighthouseBnB Reservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -36,13 +36,7 @@ const getUserWithEmail = function (email) {
   let user;
 
   return pool.query(queryString, values)
-    .then(user => {
-      if (res.rows[0] !== {}) {
-        user = res.rows[0];
-      } else {
-        user = null;
-      }
-    });
+    .then(res => res.rows[0]);
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -92,7 +86,19 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const queryString = `
+  SELECT reservations.*, properties.*, avg(property_reviews.rating) AS average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON property_reviews.reservation_id = reservations.id
+  WHERE reservations.end_date < now()::date AND reservations.guest_id = $1
+  GROUP BY reservations.id, properties.id
+  ORDER BY reservations.start_date
+  LIMIT $2`;
+  const values = [guest_id, limit];
+
+  return pool.query(queryString, values)
+    .then(res => res.rows);
 }
 exports.getAllReservations = getAllReservations;
 
